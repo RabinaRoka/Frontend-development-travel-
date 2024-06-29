6af4e38a90cf78a43d645157708dc86609c58485
RabinaRokaa <np03cs4a210039@heraldcollege.edu.np>
2024-06-29 16:24:11 +0545

    trek details html and css

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+document.getElementById('instant-book').addEventListener('click', function() {
+    alert('Instant booking is not available right now.');
+});
+
+document.getElementById('check-availability').addEventListener('click', function() {
+    alert('Checking availability...');
+});
